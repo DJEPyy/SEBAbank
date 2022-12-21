4aa710177b7754c160bc1983d575d0739c651a18
schon <schon.jesper@gmail.com>
2022-12-21 14:47:21 +0100

    Initial commit

--- src/Bank.java
+++ src/Bank.java
@@ -1,2 +1,27 @@
+<<<<<<< HEAD
 public class Bank {
+=======
+import java.util.HashMap;
+import java.util.Map;
+
+public class Bank {
+
+    private Map<String, Account> accounts;
+
+    public Bank() {
+        accounts = new HashMap<>();
+    }
+
+    public void createAccount(String name, double balance) {
+        accounts.put(name, new Account(balance));
+    }
+
+    public void addAccount(String name, Account account) {
+        accounts.put(name, account);
+    }
+
+    public Account getAccount(String name) {
+        return accounts.get(name);
+    }
+>>>>>>> Initial commit
 }
