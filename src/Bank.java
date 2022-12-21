f6147638b86b5da6e371c960c4aa026609025525
schon <schon.jesper@gmail.com>
2022-12-21 14:47:21 +0100

    Initial commit

--- src/Bank.java
+++ src/Bank.java
@@ -1,3 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
 
+    private Map<String, Account> accounts;
+
+    public Bank() {
+        accounts = new HashMap<>();
+    }
+
+    public void createAccount(String name, double balance) {
+        accounts.put(name, new Account(balance));
+    }
+
+    public void addAccount(String name, Account account) {
+        accounts.put(name, account);
+    }
+
+    public Account getAccount(String name) {
+        return accounts.get(name);
+    }
 }
