d1df5953c2954ba7b77737986369675564084f90
pmodouf <pmodouf@gmail.com>
2022-12-21 16:17:23 +0100

    addedFacade

--- src/BankFacade.java
+++ src/BankFacade.java
@@ -1,2 +1,28 @@
 public class BankFacade {
-}
+    private Bank bank;
+
+    public BankFacade(Bank bank) {
+        this.bank = bank;
+    }
+
+
+    public void createAccount(String name, double balance) {
+        bank.createAccount(name, balance);
+    }
+
+    public void transfer(String fromAccountName, String toAccountName, double amount) {
+        Account fromAccount = bank.getAccount(fromAccountName);
+        Account toAccount = bank.getAccount(toAccountName);
+        fromAccount.withdraw(amount);
+        toAccount.deposit(amount);
+    }
+
+    public void payBill(String accountName, double amount) {
+        Account account = bank.getAccount(accountName);
+        account.withdraw(amount);
+    }
+
+    public double getBalance(String accountName) {
+        Account account = bank.getAccount(accountName);
+        return account.getBalance();
+    }}
